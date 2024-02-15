d8dae10b830b3e8bb4cf31a404e8a02aa69b9182
gtamaral <rodrigoamaral003@gmail.com>
2024-02-15 16:51:29 -0300

    feat: add tags/badge & new projects

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { Badge } from "@/components/ui/badge";
 
-const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
+const ProjectCard = ({ imgUrl, title, tags, description, gitUrl, previewUrl }) => {
   return (
     <div>
       <div
         className="h-52 md:h-72 rounded-t-xl relative group"
-        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
+        style={{ background: `url(${imgUrl})`, backgroundSize: "cover", backgroundPosition: "center" }}
       >
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link
             href={gitUrl}
             className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
-            <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+            <CodeBracketIcon className="h-10 w-10 text-[#4188bb] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
           </Link>
           <Link
             href={previewUrl}
@@ -26,7 +27,13 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
       </div>
       <div className="text-white rounded-b-xl mt-2 bg-[#181818]py-6 px-4">
         <h5 className="text-xl font-semibold mb-2">{title}</h5>
-        <p className="text-[#ADB7BE]">{description}</p>
+        {/* add badge stacs */}
+        <div className=" flex flex-wrap gap-2 w-full pb-2">
+          {tags.map((tag) => <Badge  className="bg-blue-950  h-5" key={tag}>{tag}</Badge>)}
+          {/* <Badge className="h-6 w-14 bg-">{tags.map((tag) => ())}</Badge> */}
+        </div>
+
+        <p className="text-[#ADB7BE] mt-2">{description}</p>
       </div>
     </div>
   );
--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -7,47 +7,80 @@ import ProjectCard from "./ProjectCard";
 const projectsData = [
     {
         id: 1,
+        title: "BarberApp",
+        tags: ["NextJs", "typescript", "TailwindCSS", "PostgreSQL", "GoogleAuth" ],
+        description: "Aplicação responsiva para agendamentos de serviços de barbearias.",
+        image: "/images/projects/barberapp.png",
+        gitUrl: "https://github.com/gtamaral/fsw-barberapp",
+        previewUrl: "https://fullstackweek-barber.vercel.app/",
+    },
+    {
+        id: 2,
+        title: "burgerapp",
+        tags: ["React Native", "TailwindCSS", "Typescript", "Zustand"],
+        description: "Aplicativo que fornece o cardápio de uma lanchonete e permite que o usuário realize o pedido à hamburgueria através do whatsapp.",
+        image: "/images/projects/burgerappedit.png",
+        gitUrl: "https://github.com/gtamaral/burgerapp",
+        previewUrl: "https://gtgram.onrender.com/",
+    },
+    {
+        id: 4,
+        title: "Portfólio",
+        tags: ["Next.js", "TailwindCSS"],
+        description: "Portfólio desenvolvido com Nextjs e TailwindCSS.",
+        image: "/images/projects/portfolio2.png",
+        gitUrl: "https://github.com/gtamaral/portfolio-nextjs",
+        previewUrl: "https://gtamaral.vercel.app/",
+    },
+    {
+        id: 4,
         title: "gtgram",
-        description: "Aplicação de Rede Social para compartilhamento de fotos com React, Node, MongoDB e Express.",
+        tags: ["Reactjs", "Nodejs", "Express", "mongoDb"],
+        description: "Aplicação de Rede Social para compartilhamento e interação(curtidas e comentários) de fotos",
         image: "/images/projects/gtgram.png",
         gitUrl: "https://github.com/gtamaral/gtGram-frontend",
         previewUrl: "https://gtgram.onrender.com/",
     },
     {
-        id: 2,
+        id: 5,
         title: "About Movies",
+        tags: ["Reactjs", "API", "CSS"],
         description: "Aplicação desenvolvida em React para fornecer informações sobre filmes usando a API TMDB como fonte de dados.",
         image: "/images/projects/movie.png",
         gitUrl: "https://github.com/gtamaral/AboutMovies",
         previewUrl: "https://about-movies-git-master-gtamaral.vercel.app/",
     },
     {
-        id: 3,
+        id: 6,
         title: "Budget",
+        tags: ["ReactJs", "CSS"],
         description: "Aplicativo web em React para acompanhar orçamento, registrar despesas e calcular saldo, oferecendo controle financeiro de maneira intuitiva.",
         image: "/images/projects/carteira.png",
         gitUrl: "https://github.com/gtamaral/carteira-digital",
         previewUrl: "https://carteira-digital-chi.vercel.app/",
     },
     {
-        id: 4,
+        id: 7,
         title: "Omnifood",
+        tags: ["HTML", "CSS"],
         description: "Landing page em HTML e CSS para um aplicativo de entrega de comida, garantindo uma experiência ao usuário como layouts atrativos e responsivos.",
         image: "/images/projects/food.png",
         gitUrl: "https://github.com/gtamaral/omnifood-study-css",
         previewUrl: "https://gtamaral.github.io/omnifood-study-css/",
     },
     {
-        id: 5,
+        id: 8,
         title: "Secret word",
+        tags: ["Reactjs", "CSS"],
         description: "O projeto em React envolve adivinhar palavras a partir de dicas fornecidas, com uma base de dados categorizada por sugestões, desafiando os jogadores a revelar a palavra letra por letra.",
         image: "/images/projects/palavra.png",
         gitUrl: "https://github.com/gtamaral/Secret-Word",
         previewUrl: "https://secret-word-gamma.vercel.app/",
     },
     {
-        id: 6,
+        id: 9,
         title: "To Do com SQL",
+        tags: ["Reactjs", "CSS"],
         description: " Gerenciamento de tarefas construído em Node.js e arquitetura MVC. ",
         image: "/images/projects/todo.png",
         gitUrl: "https://github.com/gtamaral/ToDo-SQL",
@@ -59,10 +92,10 @@ const projectsData = [
 const ProjectsSection = () => {
   return (
     <section id='projects'>
-    <div className='w-70 flex self-center sm:justify-center justify-center text-center'>
+    <div className='w-70 flex self-center sm:justify-center justify-center text-center pt-20'>
         <h2 className='rounded-full border-2 border-blue-800 px-6 py-6 text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12  '>Projetos:</h2>
     </div>
-        <div className='grid md:grid-cols-3 gap-8 md:gap-12'>{projectsData.map((project) => <ProjectCard key={project.id} title={project.title} description={project.description} imgUrl={project.image} gitUrl={project.gitUrl} previewUrl={project.previewUrl}/>)}</div>
+        <div className='grid md:grid-cols-3 gap-8 md:gap-12'>{projectsData.map((project) => <ProjectCard key={project.id} title={project.title} description={project.description} tags={project.tags} imgUrl={project.image} gitUrl={project.gitUrl} previewUrl={project.previewUrl}/>)}</div>
     </section>
   )
 }
